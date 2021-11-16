fb27f64c9cf367a2ac96f9b2f84728562273a23c
Ethan <ethan@koldfusion.xyz>
2021-11-17 00:34:25 +1300

    Pypi things

---
+++ economy/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.1a1"
+
+import sys
+import logging
+from collections import namedtuple
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=0, minor=0, micro=1, releaselevel="alpha", serial=0)
+
+
+if sys.version_info[1] < 8:
+    raise RuntimeError("This package requires python 3.8 or higher.")
